3f3fcd553b96c8914f439ad28b5b73cffa2c0e8c
Naramsim <igougi.ui@gmail.com>
2015-07-07 18:16:52 +0200

    Added GitHub "Fork Me"

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Log to Histooy</title>
+		<title>Log to History</title>
 		<!-- Font Google Open Sans -->
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet' type='text/css'>
 		<link href='css/tabs.css' rel='stylesheet' type='text/css'>	
@@ -16,5 +16,8 @@
 				</ul>
 			</nav>
 		</div>
+		<a href="https://github.com/Naramsim/Log-to-History">
+	    	<img style="position: absolute; top: -7; right: 0; border: 0; height: 21%; transform: rotate(-4deg);" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png">
+	    </a>
 	</body>
 <html>
